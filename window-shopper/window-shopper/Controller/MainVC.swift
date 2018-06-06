5f238356e3a6c3b430223958bcf989a7632ad3d8
Nadila Dithmal Kulanatha <nadiladithmal@gmail.com>
2018-06-06 18:55:32 +0530

    Custom View changes now appear in the interface builder, setted up text fields and added a button to open with the activation of any text field

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    //create IBOutlets for two text fields
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60)) //create a button
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5945168734, blue: 0.00580148818, alpha: 1)     //assign a color to the button
+        calcBtn.setTitle("Calculate", for: .normal)     //set text of the button
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)     //set text color for the button
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside) //create target for the button
+        
+        // .inputAccessoryView activates the calcBtn
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func calculate() {
+        print("We got here") //flag to identify the program entering calculate function
     }
 
 
--- window-shopper/window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -7,22 +7,27 @@
 //
 
 import UIKit
+@IBDesignable
 
 class CurrencyTextField: UITextField {
-
+    
+    override func prepareForInterfaceBuilder() {    //  calls the prepare for interface builder prebuilt function
+        customizedView()
+    }
+    
     override func awakeFromNib() {
+        customizedView()                            // calls the custom view code
+    }
+    func customizedView() {                         // custom view code inside a special funciton
         super.awakeFromNib()
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
-        if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        if let p = placeholder { // if let sequence to safely unwrap the optional 'placeholder'
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]) //create constant 'place' with the object 'NSAttributedString'
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
     }
-
 }
